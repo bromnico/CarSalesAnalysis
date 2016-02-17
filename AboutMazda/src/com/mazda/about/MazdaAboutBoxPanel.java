6c98ba4791a328af06682816665e5a893189aac8
Brent <brent@romneyplanet.com>
2016-02-16 23:14:03 -0700

    Initial Commit

---
+++ AboutMazda/src/com/mazda/about/MazdaAboutBoxPanel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mazda.about;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author brent
+ */
+public class MazdaAboutBoxPanel extends JPanel{
+    
+}
